755825e5c594b909ada3b2b7d0bc32592e47ee8c
unknown <joemccole12@gmail.com>
2024-10-07 14:34:01 +0000

    Week 3

--- my-movie-app/src/Components/movies.js
+++ my-movie-app/src/Components/movies.js
@@ -3,7 +3,7 @@ import MovieItem from "./movieItem";
 const Movies = (props)=>{//Arrow Function
     
     return props.myMovies.map(//using the map function to assign to each element of array
-        (movie)=>{return <MovieItem myMovie = {movie}/>}
+        (movie)=>{return <MovieItem myMovie = {movie} key = {movie.imdbID}/>}
     );
 }
 export default Movies;//exporting movies to be used in read
