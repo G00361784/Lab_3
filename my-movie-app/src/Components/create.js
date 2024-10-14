422c33ac61de6fa6c85bea90fe076d5b28c85e1d
unknown <joemccole12@gmail.com>
2024-10-14 14:24:36 +0000

    main

--- my-movie-app/src/Components/create.js
+++ my-movie-app/src/Components/create.js
@@ -1,7 +1,54 @@
-import React from "react";//importing react
-const Create = ()=>{//Arrow Function
-    return(
-        <h3 className="text-center">Hello from Create</h3>
-    );
+// create.js
+
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState('');
+  const[Year, setYear] = useState('');//using usestate
+  const[Poster, setPoster] = useState('');
+  const handleSubmit = (e) => {//using handle submit function
+    e.preventDefault();
+
+    console.log($[title],$[Year],$[Poster]);//console.loging title
+    
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>{/**calling handlesubmit to handle the event  */}
+        
+        
+        <div className="form-group">
+          <label>Add Movie Title: </label>{/**adding movie to list */}
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Add Movie Year: </label>{/**adding year to list */}
+          <input type="text"
+            className="form-control"
+            value={Year}
+            onChange={(e) => { setYear(e.target.value) }}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Add Movie Poster: </label>{/**adding poster top list */}
+          <input type="text"
+            className="form-control"
+            value={Poster}
+            onChange={(e) => { setPoster(e.target.value) }}
+          />
+        </div>
+        <input type="submit" value="Add Movie" />{/**Using submit to handle form */}
+      </form>
+    </div>
+  );
 }
+
 export default Create;
